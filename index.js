ce7b804e546bb117d78c7212b21b0ffcf9ea6820
ekinma99 <divinegrace38@gmail.com>
2023-01-16 16:47:02 -0500

    Add Names; Minor fixes

--- index.js
+++ index.js
@@ -33,26 +33,26 @@ function firstNames(){
 }
 }
 function lastNames(){
-    let last_names = ['Adams', 'Johnson', 'Smith', 'Carter','Santos', 'Perez', 'Haddad', 'Woods', 'Brown', 'Reid', 'Jones', 'Reynolds', 'Simpson', 'Brooks', 'Gardner', 'Clancy', 'Banks', 'Dawson','Cooper', 'Winterforth','Samson', 'Murphy','Crimson', 'Butterfield', 'Cartier', 'Benson', 'Johnson', 'Richardson', 'Holland', 'Neumann', 'Woods', 'Cooper', 'Wheaton', 'Douglas']
+    let last_names = ['Adams', 'Johnson', 'Smith', 'Carter','Santos', 'Perez', 'Haddad', 'Woods', 'Brown', 'Reid', 'Jones', 'Reynolds', 'Simpson', 'Brooks', 'Gardner', 'Clancy', 'Banks', 'Dawson','Cooper', 'Winterforth','Samson', 'Murphy','Crimson', 'Butterfield', 'Cartier', 'Benson', 'Johnson', 'Richardson', 'Holland', 'Neumann', 'Woods', 'Cooper', 'Wheaton', 'Douglas', 'Nicks', 'Maxwell']
     let last_name = last_names[Math.floor(Math.random()*last_names.length)]
     localStorage.setItem("Last Name", last_name)
     window.location = 'lastName.html'
 }
 function femaleNames(){
-    let female_names = ['Sarah', 'Audrey', 'Samantha', 'Blossom', 'Amy', 'Kelly', 'Kimberly', 'Diana', 'Diane', 'Lisa', 'Justine', 'Betty', 'Anna', 'Sabrina', 'Jaya', 'Brianna', 'Penny', 'Kathrine', 'Paola', 'Cristina', 'Kristen', 'Julia', 'Stacey', 'Tiffany', 'Emma', 'Jane', 'Venus', 'Sarah', 'Kylie', 'Alicia', 'Brittney', 'Nicole', 'Jasmine', 'Courtnee']
+    let female_names = ['Sarah', 'Audrey', 'Samantha', 'Blossom', 'Amy', 'Kelly', 'Kimberly', 'Diana', 'Diane', 'Lisa', 'Justine', 'Betty', 'Anna', 'Sabrina', 'Jaya', 'Brianna', 'Penny', 'Kathrine', 'Paola', 'Cristina', 'Kristen', 'Julia', 'Stacey', 'Tiffany', 'Emma', 'Jane', 'Venus', 'Sarah', 'Kylie', 'Alicia', 'Brittney', 'Nicole', 'Jasmine', 'Courtnee', 'Marvella', 'Patricia','Joy', 'Annabelle', 'Terra', 'Joyce', 'Alexa', 'Dakota','Orlandi','Sierra','Bria','Milly','Ceclia','Amanda', 'Jennifer', 'Tessa', 'Tierra', 'Cantarose', 'Katia', 'Leah', 'Carrie', 'Candance','Rita', 'Nita', 'Sydney', 'Sapphire', 'Pearl', 'Ruby', 'Trisha', 'Brandi', 'Lois' ,'Hailey', 'Cassie', 'Aisha']
         let female_name = female_names[Math.floor(Math.random()*female_names.length)] 
         localStorage.setItem("Female Name", female_name)       
         window.location = 'femaleName_result.html'
 }
 function maleNames(){
-    let male_names = ['Jake','Braxton', 'Timothy', 'Victor', 'Simon', 'Bradley', 'Millard', 'William', 'Harry', 'Christopher', 'Richard', 'Johnathan', 'Nathan', 'Willard', 'Nicolas', 'Joshua', 'David', 'Luke', 'Peter', 'Brian', 'Carlos', 'Miguel', 'Frank', 'Christoph', 'Marco', 'Marcus', 'Nikola', 'Jason', 'Hector', 'Julius', 'Donald', 'Andrew', 'Drew', 'Zayn', 'Garrett', 'Arnold', 'Carl', 'Sebastien', 'Leon', 'Ethan', 'Ezra', 'Jesse', 'Jared', 'Jonah', 'Gideon', 'Asher']
+    let male_names = ['Jake','Braxton', 'Timothy', 'Victor', 'Simon', 'Bradley', 'Millard', 'William', 'Harry', 'Christopher', 'Richard', 'Johnathan', 'Nathan', 'Willard', 'Nicolas', 'Joshua', 'David', 'Luke', 'Peter', 'Brian', 'Carlos', 'Henry', 'Frank', 'Christoph', 'Marco', 'Marcus', 'Nikola', 'Jason', 'Hector', 'Julius', 'Donald', 'Andrew', 'Drew', 'Zayn', 'Garrett', 'Arnold', 'Carl', 'Sebastien', 'Leon', 'Ethan', 'Ezra', 'Jesse', 'Jared', 'Jonah', 'Gideon', 'Asher', 'Gavin', 'Keanu', 'Benjamin', 'Kurt','Todd', 'Philip', 'Mark', 'Dillon','Steven', 'Krish', 'Maxwell', 'Cory']
     let male_name = male_names[Math.floor(Math.random()*male_names.length)]
     localStorage.setItem("Male Name", male_name)
     window.location = 'maleName_result.html'
 
 }
 function nonBinary(){
-    let non_binary_names = ['Sam', 'Carter', 'Chris', 'Dylan', 'Julian', 'Cameron', 'Charlie', 'Blake', 'Tatum', 'Riley', 'Pat', 'Parker',' Max', 'Ryan', 'Bailey', 'Kris', 'Murphy', 'Logan', 'Chandler', 'Juno']
+    let non_binary_names = ['Sam', 'Carter', 'Chris', 'Dylan', 'Julian', 'Cameron', 'Charlie', 'Blake', 'Tatum', 'Riley', 'Pat', 'Parker',' Max', 'Ryan', 'Bailey', 'Kris', 'Murphy', 'Logan', 'Chandler', 'Juno', 'Kelly', 'Alexis', 'Margo', 'Spencer','Marvel', 'Rye', 'Nico', 'Kelsey', 'Amory']
     let non_binary_name = non_binary_names[Math.floor(Math.random()*non_binary_names.length)]
     localStorage.setItem("Non Binary Name",  non_binary_name)
     window.location = 'nonbinaryName_result.html'
